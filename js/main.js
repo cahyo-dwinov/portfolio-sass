ba185fd5f5a4c1e7ead885e0d1b3bfb2cc2d8ee3
Cahyo Dwinov <59456940+cahyo-dwinov@users.noreply.github.com>
2020-09-06 22:15:51 +0700

    progress on nav

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn__burger')
+const hamburger = document.querySelector('.menu-btn__burger');
+
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav')
 
 let showMenu = false;
 
@@ -9,10 +12,14 @@ function toogleMenu() {
     if (!showMenu) {
         hamburger.classList.add('open');
 
+        nav.classList.add('open');
+        menuNav.classList.add('open');
         showMenu = true;
     } else {
         hamburger.classList.remove('open');
 
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
 
         showMenu = false;
     }
